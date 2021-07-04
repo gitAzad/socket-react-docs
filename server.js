99ad1500cef0df2a99ca679bc421603f39c220d4
gitAzad <mo.azad1999@gmail.com>
2021-07-04 12:06:42 +0530

    n4

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
